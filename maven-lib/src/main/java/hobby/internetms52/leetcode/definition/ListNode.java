958fb5cf0381109dc6a60feea5f8098ca5e26717
internetms52 <internetms52@gmail.com>
2025-02-24 20:43:22 +0800

    update

---
+++ maven-lib/src/main/java/hobby/internetms52/leetcode/definition/ListNode.java
@@ -0,0 +1,31 @@
+package hobby.internetms52.leetcode.definition;
+
+public class ListNode implements Comparable<ListNode> {
+    int val;
+    public ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    @Override
+    public int compareTo(ListNode o) {
+        if (val == o.val) {
+            if (next != null && o.next != null) {
+                return next.compareTo(o.next);
+            }
+            if (next == null && o.next == null) {
+                return 0;
+            }
+        }
+        return -1;
+    }
+}
